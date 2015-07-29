78f11dddeefa79f7f0df15fea1cf1c2f8248b469
Adam <adam.kinson@gmail.com>
2015-07-29 08:31:33 -0400

    build Passengers controller and routes

--- transportation/config/routes.rb
+++ transportation/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  resources :trains do
+    resources :passengers
+  end
+
+  resources :airplanes do
+    resources :passengers
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
