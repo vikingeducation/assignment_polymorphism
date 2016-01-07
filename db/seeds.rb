dc03fe36c5fdb5add4a5dbac962d0569bae18206
Sia <siakaramalegos@gmail.com>
2016-01-07 16:41:28 -0600

    Adds faker gem and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Vote.destroy_all
+Buzz.destroy_all
+Smart.destroy_all
+User.destroy_all
+
+20.times do
+  User.create(name: Faker::Internet.user_name)
+  Buzz.create(phrase: Faker::Company.catch_phrase)
+  Smart.create(phrase: Faker::Hacker.say_something_smart)
+end
+
+100.times do
+  user_id = User.pluck(:id).sample
+  if rand(0..1) == 1
+    object_id = Buzz.pluck(:id).sample
+    object_type = 'Buzz'
+  else
+    object_id = Smart.pluck(:id).sample
+    object_type = 'Smart'
+  end
+
+  Vote.create(user_id: user_id, votable_id: object_id, votable_type: object_type)
+end
+
+puts 'Done seeding!'
