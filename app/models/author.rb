e2906ffa5e76c7cf84bf70d87bee151afab04708
Thomas Lo <thomasjinlo@gmail.com>
2016-02-09 11:40:04 -0800

    finish associations for polymorph

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,5 +2,9 @@ class Author < ActiveRecord::Base
 
   has_many :articles
   has_many :comments
+  has_many :tagged_articles, through: :articles,
+            source: :tags
 
+  has_many :tagged_comments, through: :comments,
+            source: :tags
 end
