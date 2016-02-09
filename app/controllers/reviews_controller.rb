898cd7d173dcdaa1b074589fa3d5b83a83108467
Andrew Baik <and.baik@gmail.com>
2016-02-09 18:53:13 -0500

    finished view/bug

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,16 @@
 class ReviewsController < ApplicationController
 
   def index
-    @parent = params[:reviewable].constantize
-    @reviews = Review.where("reviewable_type = #{@parent}").where("reviewable_id" = "#{}")
+    reviewable_id = get_reviewable_id
+    @reviewable = params[:reviewable].constantize.find(reviewable_id)
+    @reviews = @reviewable.reviews
+  end
+
+  private
+
+  def get_reviewable_id
+    class_name = params[:reviewable]
+    class_id = (class_name.downcase + "_id").to_sym
+    params[class_id]
   end
 end
