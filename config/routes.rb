18e9d28fdd3166264ebf20d0fb346438d9476b59
Steven Chang <prime_pork@hotmail.com>
2016-04-13 13:12:31 +1000

    added a defaults option to the items routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   resources :restaurants do
-    resources :items
+    resources :items, :defaults => { :commentable => 'Restaurant' }
   end
 
   resources :vending_machines do
-    resources :items
+    resources :items, :defaults => { :commentable => 'VendingMachine' }
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
