772d0bb6741936cb4f8b6ccc104f912301310ece
Steven Chang <prime_pork@hotmail.com>
2016-04-13 12:14:10 +1000

    created tables for items, restaurants and vending_machines

--- db/migrate/20160413020751_create_vending_machines.rb
+++ db/migrate/20160413020751_create_vending_machines.rb
@@ -1,6 +1,8 @@
 class CreateVendingMachines < ActiveRecord::Migration
   def change
     create_table :vending_machines do |t|
+      t.string :latitude, :null => false
+      t.string :longitude, :null => false
 
       t.timestamps null: false
     end
