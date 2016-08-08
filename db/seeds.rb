9378c904226aa07d6186de5aa9fcd98e56c4f6ff
Mike Lee <asackofwheat@gmail.com>
2016-08-08 14:32:47 -0400

    working on front-end

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+NAMES = ["John Smith", "Chris Evert", "Ricky Martin", "Yan Rickshaw", "Bob Roberts", "John Wayne"]
+
+puts "creating countries"
+Country.create!(:name => "USA")
+Country.create!(:name => "China")
+Country.create!(:name => "Russia")
+
+puts "creating sports"
+Sport.create!(:name => "Baseball")
+Sport.create!(:name => "Basketball")
+Sport.create!(:name => "Football")
+
+puts "creating athletes"
+Country.all.each_with_index do |country, index|
+  country.athletes.create!(:name => NAMES[index])
+end
+
+puts "creating athletes"
+Sport.all.each_with_index do |sport, index|
+  sport.athletes.create!(:name => NAMES[index+3])
+end
