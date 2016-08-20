038c2db813ebd7f0ab350966e465df438fa9be6f
schoetlr <srlrs20020@gmail.com>
2016-08-20 12:55:53 -0400

    all working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :photos do 
+    resources :likes
+  end
+
+  resources :posts do 
+    resources :likes
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
