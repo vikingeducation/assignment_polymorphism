558774da9a751048bf79c7d3d4f8800cc488ccbe
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2016-12-05 14:59:53 -0500

    Begin implementing basic front end

---
+++ app/controllers/engines_controller.rb
@@ -0,0 +1,5 @@
+class EnginesController < ApplicationController
+  def index
+    #@engines = Engine.where(drivable_type: , drivable_id: )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'engines/index'
+
+  resources :cars do
+    resources :engines
+  end
+
+  resources :motorcycles do
+    resources :engines
+  end
 end
