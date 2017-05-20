81860615a1e09d762df46154d21a2d67ee1ddb81
Visiona <darbis@gmail.com>
2017-05-20 20:49:59 +0100

    Created simple index and routes to experiment with polymorphic comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+
+  resources :photos do
+    resources :comments, :defaults => { :commentable => 'Photo' }
+  end
+
+  resources :articles do
+    resources :comments, :defaults => { :commentable => 'Post' }
+  end
+
 end
