ae7cc91b5f704b077792b9f0af03b83258d64602
anne <richardson.ae@gmail.com>
2017-12-25 16:02:24 -0600

    Builds out controllers for polym parents and children

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :applicants do
+    resources :tags, only: [:index], :defaults => { :taggable => 'Applicant' }
+  end
+
+  resources :jobs do
+    resources :tags, only: [:index], :defaults => { :taggable => 'Job' }
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
